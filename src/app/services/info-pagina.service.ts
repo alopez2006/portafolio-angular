c0e574109503e4e816f42b54c89b15073408ac30
Arturo López <alopez2006@gmail.com>
2018-09-03 13:24:09 -0500

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoPagina, Equipo } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,15 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: Array<Equipo> = [];
+
   // private http:HttpClient para peticiones REST, servicios externos
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer archivo json
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
@@ -22,4 +29,14 @@ export class InfoPaginaService {
         console.log(resp.facebook);
       });
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-portafolio-4b927.firebaseio.com/equipo.json')
+      .subscribe((resp: Array<Equipo>) => {
+        this.equipo = resp;
+        console.log(resp);
+        console.log(resp[0].nombre);
+      });
+  }
+
 }
